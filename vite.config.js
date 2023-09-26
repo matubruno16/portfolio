a10ff1e0a5a1211096e79b5ec4798ca2c606e0b2
matubruno16 <matubruno16@gmail.com>
2023-09-26 15:15:33 -0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://matubruno16.github.io/portfolio',
 })
